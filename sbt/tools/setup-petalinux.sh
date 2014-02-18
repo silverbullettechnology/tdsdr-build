d5cd7eb488a853495d2f1dcfb1de7c87e3f3a1b5
Morgan Hughes <morgan.hughes@silver-bullet-tech.com>
2014-02-17 21:48:00 -0800

    Fix a bug when directory missing
    Missing files dir would cause the entire tree to be copied into the petalinux root

--- sbt/tools/setup-petalinux.sh
+++ sbt/tools/setup-petalinux.sh
@@ -52,7 +52,7 @@ fi
 
 /bin/sh "$SBT_TOOLS/setup-petalinux-${SBT_PETALINUX_VER}.sh" || exit 1
 
-(cd "$SBT_FILES/petalinux-$SBT_PETALINUX_VER"; cp -av * "$SBT_PETALINUX")
+(cd "$SBT_FILES/petalinux-$SBT_PETALINUX_VER" && cp -av * "$SBT_PETALINUX")
 
 ls -1 "$SBT_PATCHES/petalinux-$SBT_PETALINUX_VER" | while read p; do
 	if ! cat "$SBT_PATCHES/petalinux-$SBT_PETALINUX_VER/$p" | \
@@ -63,9 +63,6 @@ ls -1 "$SBT_PATCHES/petalinux-$SBT_PETALINUX_VER" | while read p; do
 done
 
 touch "$SBT_PETALINUX/.sbt-patched" 
-
-
-
 
 exit 0
 
