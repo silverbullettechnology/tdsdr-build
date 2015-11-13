192d03cf8221f58a7321098534bbe74cf05f82dd
Morgan Hughes <morgan.hughes@silver-bullet-tech.com>
2015-11-12 18:27:04 -0800

    Mantis 183: fix hexdump of multiple packets
    Reopen the controlling TTY for stdout/stderr so that setting stdin non-blocking doesn't
    affect them.

--- sbt/user-apps/srio-tool/st_main.c
+++ sbt/user-apps/srio-tool/st_main.c
@@ -224,8 +224,6 @@ int main (int argc, char **argv)
 				break;
 		}
 
-//	setbuf(stdout, NULL);
-
 	int dev = open(DEV_NODE, O_RDWR|O_NONBLOCK);
 	if ( dev < 0 )
 	{
@@ -235,6 +233,16 @@ int main (int argc, char **argv)
 
 	signal(SIGTERM, signal_fatal);
 	signal(SIGINT,  signal_fatal);
+
+	// freopen the controlling terminal for stdout/stderr before st_term_setuip(), so
+	// setting stdin non-blocking doesn't also affect stdout
+	char tty[256];
+	if ( readlink("/proc/self/fd/1", tty, sizeof(tty)) > 0 )
+	{
+		freopen(tty, "w", stdout);
+		freopen(tty, "w", stderr);
+		setbuf(stderr, NULL);
+	}
 
 	st_term_setup();
 
